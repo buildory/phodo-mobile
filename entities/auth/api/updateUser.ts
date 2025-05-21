933512a1d9fcd15ec6de98edc9170a3f96601c21
jwjeon <buildory.dev@gmail.com>
2025-05-21 14:24:58 +0900

    feat: auth 관련 api 추가

---
+++ entities/auth/api/updateUser.ts
@@ -0,0 +1,20 @@
+import { supabase } from "@/shared/lib/supabase";
+
+export type UpdateUserPayload = {
+  email?: string;
+  password?: string;
+  data?: Record<string, any>;
+  emailRedirectTo?: string;
+};
+
+export const updateUser = async ({
+  email,
+  password,
+  data,
+  emailRedirectTo,
+}: UpdateUserPayload) => {
+  const attributes = { email, password, data };
+  const options = emailRedirectTo ? { emailRedirectTo } : {};
+
+  return await supabase.auth.updateUser(attributes, options);
+};
