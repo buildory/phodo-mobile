746ad97eec9b4fee14952a1f037d14e6e73bf6fa
jwjeon <buildory.dev@gmail.com>
2025-06-18 18:32:03 +0900

    feat: useProject export

--- entities/projects/model/index.ts
+++ entities/projects/model/index.ts
@@ -1,3 +1,4 @@
 export { Project, ProjectListParams } from "./project.types";
 export { useProjects } from "./useProjects";
+export { useProject } from "./useProject";
 export { CategorySchema, DeviceSchema, ProjectSchema, ProfileSchema, ProjectListParamsSchema } from "./project.schema";
