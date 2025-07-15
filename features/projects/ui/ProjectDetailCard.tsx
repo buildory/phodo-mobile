26d65fc0bf46b4e2711834bbc0a0c6f64772d5f5
jwjeon <buildory.dev@gmail.com>
2025-07-16 01:06:54 +0900

    update: 촬영 상세 카드 디자인 적용

--- features/projects/ui/ProjectDetailCard.tsx
+++ features/projects/ui/ProjectDetailCard.tsx
@@ -1,7 +1,10 @@
-import { View, Text, StyleSheet, Pressable } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import Badge from "@/shared/ui/Badge";
 import { copyToClipboard } from "@/shared/lib/clipboard";
 import { IconSymbol } from "@/shared/ui/IconSymbol";
+import dayjs from "dayjs";
+import "dayjs/locale/ko";
+dayjs.locale("ko");
 
 export const COOPERATION_OPTIONS = [
   "thanks",
@@ -60,234 +63,146 @@ const SHOOTING_PREFERENCE_LABELS: Record<ShootingPreference, string> = {
   model_device: "모델님 기기로 촬영 가능해요",
 };
 
+const daysMap = {
+  mon: "월",
+  tue: "화",
+  wed: "수",
+  thu: "목",
+  fri: "금",
+  sat: "토",
+  sun: "일",
+};
+
+function formatTime(timeString: string) {
+  if (!timeString) return "시간 없음";
+
+  const parsed = dayjs(`2000-01-01T${timeString}`);
+
+  if (!parsed.isValid()) return "잘못된 시간";
+
+  return parsed.locale("ko").format("A h:mm");
+}
+
 export default function ProjectDetailCard({ project }) {
   return (
-    <View
-      className="card"
-      style={{
-        borderColor: "#E9EAEB",
-        borderWidth: 1,
-        borderRadius: 8,
-        padding: 12,
-        flex: 1,
-        gap: 8,
-      }}
-    >
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          gap: 6,
-        }}
-      >
+    <View className="p-12 gap-8 flex-1">
+      <View className="flex flex-row items-center gap-6">
         <Badge
           label={project.recruitType === "model" ? "모델 구인" : "작가 구인"}
-          backgroundColor="#fff"
-          color="#6172f3"
-          borderColor="#6172f3"
-          borderRadius={4}
-          borderWidth={1}
+          className="border-stroke-field border rounded-4"
         />
         <Badge
           label={project.pinDisplay === "always" ? "상시 촬영" : "버블 촬영"}
-          backgroundColor="#fff"
-          color="#6172f3"
-          borderColor="transparent"
-          borderWidth={1}
+          className="border-stroke-field border rounded-4"
         />
       </View>
       <Pressable>
         <Text
-          numberOfLines={2}
+          numberOfLines={3}
           ellipsizeMode="tail"
-          style={{ fontWeight: "bold", fontSize: 18 }}
+          className="headline1-semiBold"
         >
           {project.title || "제목 없음"}
         </Text>
       </Pressable>
 
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <View className="flex flex-row items-center gap-6">
         <IconSymbol size={12} name="camera" color={"#717680"} />
-        <Text
-          style={{
-            color: "#535862",
-            fontSize: 14,
-            lineHeight: 18,
-            marginLeft: 4,
-          }}
-        >
-          {project.durationHours} 시간
-        </Text>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            gap: 8,
-          }}
-        >
-          <Badge
-            label={project.isPaid ? "유료" : "무료"}
-            backgroundColor="#9e77ed"
-            color="#ffffff"
-            borderRadius={4}
-          />
-          <Text style={{ color: "#9e77ed", fontWeight: 700 }}>
+        <Text className="label1-regular">{project.durationHours} 시간</Text>
+
+        <View className="flex flex-row items-center gap-8">
+          <Badge label={project.isPaid ? "유료" : "무료"} />
+          <Text className="text-fg-brand label1-semiBold">
             {project.isPaid
-              ? `${new Intl.NumberFormat("ko-KR").format(
-                  project.pricePerHour
-                )}원`
-              : COOPERATION_LABELS[project.RequestNote]?.title}
+              ? `${new Intl.NumberFormat("ko-KR").format(project.pricePerHour)}원`
+              : COOPERATION_LABELS[project.requestNote]?.title}
           </Text>
         </View>
       </View>
 
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          gap: 12,
-        }}
-      >
-        <View style={{ flexDirection: "row", gap: 12 }}>
-          <View style={{ flexDirection: "row", alignItems: "center" }}>
-            <IconSymbol size={12} name="mappin" color={"#717680"} />
-            <Text
-              style={{
-                color: "#535862",
-                fontSize: 14,
-                lineHeight: 18,
-                marginLeft: 4,
-              }}
-            >
-              {project.distance}
-            </Text>
-          </View>
-
-          <Pressable
-            onPress={() => copyToClipboard(project.locationAddress)}
-            style={{ flexDirection: "row", alignItems: "center" }}
-          >
-            <Text style={{ fontSize: 14, lineHeight: 18, color: "#535862" }}>
-              {project.inputLocation}
-            </Text>
-            <IconSymbol
-              size={12}
-              name="copy"
-              color="#000"
-              style={{ marginLeft: 4 }}
-            />
-          </Pressable>
-        </View>
+      <View className="flex flex-row items-center gap-6">
+        <IconSymbol size={12} name="mappin" color={"#717680"} />
+        {project.distance && <Text>{project.distance}</Text>}
+        <Pressable
+          onPress={() => copyToClipboard(project.locationAddress)}
+          className="flex flex-row items-center"
+        >
+          <Text className="label1-regular mr-4">{project.inputLocation}</Text>
+          <IconSymbol size={12} name="copy" color="#000" />
+        </Pressable>
       </View>
 
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <View className="flex flex-row items-center gap-6">
         <IconSymbol size={12} name="calendar" color={"#717680"} />
-        <Text
-          style={{
-            color: "#535862",
-            fontSize: 14,
-            lineHeight: 18,
-            marginLeft: 4,
-          }}
-        >
-          촬영가능 일정
-        </Text>
-        <Text
-          style={{
-            color: "#535862",
-            fontSize: 14,
-            lineHeight: 18,
-            marginLeft: 4,
-          }}
-        >
+        <Text className="label1-regular">촬영 가능 일정</Text>
+        <View className="flex flex-wrap items-center w-2/3">
+        <Text className="label1-regular">
           {project?.availableDates?.join(", ")}
-          {project?.availableDays?.join(", ")}
+          {project?.availableDays?.map((day) => ` ${daysMap[day]}`).join(", ")}
         </Text>
-      </View>
-
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <IconSymbol size={12} name="clock" color={"#717680"} />
-        <Text
-          style={{
-            color: "#535862",
-            fontSize: 14,
-            lineHeight: 18,
-            marginLeft: 4,
-          }}
-        >
-          촬영가능 시간
-        </Text>
-        <Text
-          style={{
-            color: "#535862",
-            fontSize: 14,
-            lineHeight: 18,
-            marginLeft: 4,
-          }}
-        >
-          {project.availableStartTime} ~ {project.availableEndTime}
-        </Text>
-      </View>
-
-      <View style={{ flexDirection: "row", gap: 4 }} className="category">
-        {project.projectCategories.map((categoryWrapper) => (
-          <Badge
-            key={categoryWrapper.categories.id}
-            label={categoryWrapper.categories.name}
-            backgroundColor="#fff"
-            color="#000000"
-            borderColor="#E9EAEB"
-            borderWidth={1}
-            icon={<IconSymbol size={12} name="hash" color={"#000"} />}
-          />
-        ))}
-      </View>
-      <Badge
-        label={SHOOTING_PREFERENCE_LABELS[project.deviceSource]}
-        backgroundColor="#fff"
-        color="#000000"
-        borderColor="#E9EAEB"
-        borderWidth={1}
-        icon={<IconSymbol size={12} name="hash" color={"#000"} />}
-      />
-      <View style={{ flexDirection: "row", gap: 4 }} className="category">
-        {project.projectDevices.map((deviceWrapper) => (
-          <Badge
-            key={deviceWrapper.devices.id}
-            label={deviceWrapper.devices.name}
-            backgroundColor="#fff"
-            color="#000000"
-            borderColor="#E9EAEB"
-            borderWidth={1}
-            icon={<IconSymbol size={12} name="hash" color={"#000"} />}
-          />
-        ))}
-      </View>
-      <View style={styles.separator} />
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
-        <Text>{project.profiles.nickname}</Text>
-        <View style={{ flexDirection: "row", alignItems: "center", gap: 4 }}>
-          <Text>지원자</Text>
-          <Text>0</Text>
         </View>
       </View>
-      <View style={{ flexDirection: "row", alignItems: "center", gap: 4 }}>
-        <Text>리뷰</Text>
-        <Text>0</Text>
+
+      <View className="flex flex-row items-center gap-6">
+        <IconSymbol size={12} name="clock" color={"#717680"} />
+        <Text className="label1-regular">촬영가능 시간</Text>
+        <Text className="label1-regular">
+          {formatTime(project?.availableStartTime)} ~{" "}
+          {formatTime(project?.availableEndTime)}
+        </Text>
+      </View>
+
+      <View className="flex flex-row gap-2 items-center">
+        <Text className="caption2-regular">촬영 분야</Text>
+        <View className="flex flex-row gap-2 w-2/3 flex-wrap">
+          {project.projectCategories.map((categoryWrapper) => (
+            <Badge
+              size="sm"
+              key={categoryWrapper.categories.id}
+              label={categoryWrapper.categories.name}
+              icon={<IconSymbol size={12} name="hash" color="#000" />}
+            />
+          ))}
+        </View>
+      </View>
+      <View className="flex flex-row gap-2 items-center">
+        <Text className="caption2-regular">촬영 방식</Text>
+        <View className="flex flex-row gap-2 items-center w-2/3 flex-wrap">
+          <Badge
+            label={SHOOTING_PREFERENCE_LABELS[project.deviceSource]}
+            icon={<IconSymbol size={12} name="hash" color={"#000"} />}
+          />
+        </View>
+      </View>
+      <View className="flex flex-row gap-2 items-center">
+        <Text className="caption2-regular">선호 기기</Text>
+        <View className="flex flex-row gap-2 items-center w-2/3 flex-wrap">
+          {project.projectDevices.map((deviceWrapper) => (
+            <Badge
+              key={deviceWrapper.devices.id}
+              label={deviceWrapper.devices.name}
+              size="sm"
+              variant={"outline"}
+              icon={<IconSymbol size={12} name="hash" color={"#000"} />}
+            />
+          ))}
+        </View>
+      </View>
+
+      <View className="mt-12 h-1 bg-stroke-divider-subtle" />
+      <View className="flex flex-row justify-between">
+        <Text>{project.profiles.nickname}</Text>
+        <View className="flex flex-row gap-2">
+          <View className="flex flex-row items-center gap-2">
+            <Text className="caption2-regular">지원자</Text>
+            <Text className="caption2-regular">{project.projectApplicants.length}</Text>
+          </View>
+          <View className="flex flex-row items-center gap-2">
+            <Text className="caption2-regular">리뷰</Text>
+            <Text className="caption2-regular">0</Text>
+          </View>
+        </View>
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  separator: {
-    height: 1,
-    backgroundColor: "#E0E0E0",
-    marginTop: 12,
-  },
-});
