32406f35580692455872c93ff52dca6050522748
jwjeon <buildory.dev@gmail.com>
2025-08-04 03:37:37 +0900

    feat: 지원 현황페이지 구현

---
+++ app/(tabs)/shooting-history/[id]/applicant.tsx
@@ -0,0 +1,131 @@
+import {
+  SafeAreaView,
+  StatusBar,
+  FlatList,
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+} from "react-native";
+import { useApplicants } from "@/entities/projects/model/useApplicants";
+import { Tabs, TabItem } from "@/shared/ui/Tabs";
+import { Text, View } from "react-native";
+import { router, useLocalSearchParams } from "expo-router";
+import ApplicantCard from "@/features/projects/ui/ApplicantCard";
+import CancelledProjectCard from "@/features/projects/ui/CancelledProjectCard";
+import { IconSymbol } from "@/shared/ui/IconSymbol";
+import { useProject } from "@/entities/projects/model";
+
+export default function ApplicantScreen() {
+  const { id } = useLocalSearchParams();
+  const { data: applicants } = useApplicants(Number(id));
+  const { data: project } = useProject(String(id));
+
+  const pendingApplicants = applicants?.filter(
+    (p) =>
+      p.status === "pending" || p.status === "ready" || p.status === "review"
+  );
+
+  const inProgressApplicants = applicants?.filter(
+    (p) => p.status === "shooting"
+  );
+
+  const doneApplicants = applicants?.filter((p) => p.status === "done");
+
+  const canceledApplicants = applicants?.filter((p) => p.status === "cancel");
+  return (
+    <SafeAreaView style={{ flex: 1, paddingTop: StatusBar.currentHeight }}>
+      <Pressable
+        onPress={() => router.back()}
+        className="flex flex-row gap-12 p-16"
+      >
+        <View className="mt-4">
+          <IconSymbol size={24} name="chevron.left" color={"#181D27"} />
+        </View>
+
+        <Text
+          numberOfLines={2}
+          ellipsizeMode="tail"
+          className="heading2-semiBold flex-1"
+        >
+          {project?.title}
+        </Text>
+      </Pressable>
+      <View className="bg-bg-layer-baasement flex-1">
+        <Tabs variant="underline">
+          <TabItem name="my" title="대기">
+            <KeyboardAvoidingView
+              className="flex-1"
+              behavior={Platform.OS === "ios" ? "padding" : "height"}
+            >
+              {pendingApplicants && pendingApplicants.length > 0 ? (
+                <FlatList
+                  data={pendingApplicants}
+                  keyExtractor={(item) => item.id.toString()}
+                  renderItem={({ item }) => (
+                    <ApplicantCard item={item} project={project} />
+                  )}
+                  contentContainerStyle={{
+                    gap: 20,
+                    paddingHorizontal: 8,
+                    paddingVertical: 20,
+                  }}
+                  keyboardShouldPersistTaps="always"
+                  showsVerticalScrollIndicator={false}
+                />
+              ) : (
+                <View className="flex-1 justify-center items-center">
+                  <Text className="heading1-semiBold text-fg-neutral-solid">
+                    촬영 내역이 없어요
+                  </Text>
+                </View>
+              )}
+            </KeyboardAvoidingView>
+          </TabItem>
+          <TabItem name="apply" title="진행중">
+            <View className="flex-1 justify-center items-center">
+              <Text className="heading1-semiBold text-fg-neutral-solid">
+                촬영 내역이 없어요
+              </Text>
+            </View>
+          </TabItem>
+          <TabItem name="complete" title="완료">
+            <View className="flex-1 justify-center items-center">
+              <Text className="heading1-semiBold text-fg-neutral-solid">
+                촬영 내역이 없어요
+              </Text>
+            </View>
+          </TabItem>
+          <TabItem name="cancel" title="취소">
+            <KeyboardAvoidingView
+              className="flex-1"
+              behavior={Platform.OS === "ios" ? "padding" : "height"}
+            >
+              {canceledApplicants && canceledApplicants.length > 0 ? (
+                <FlatList
+                  data={canceledApplicants}
+                  keyExtractor={(item) => item.id.toString()}
+                  renderItem={({ item }) => (
+                    <CancelledProjectCard item={item} project={project}/>
+                  )}
+                  contentContainerStyle={{
+                    gap: 20,
+                    paddingHorizontal: 8,
+                    paddingVertical: 20,
+                  }}
+                  keyboardShouldPersistTaps="always"
+                  showsVerticalScrollIndicator={false}
+                />
+              ) : (
+                <View className="flex-1 justify-center items-center">
+                  <Text className="heading1-semiBold text-fg-neutral-solid">
+                    촬영 내역이 없어요
+                  </Text>
+                </View>
+              )}
+            </KeyboardAvoidingView>
+          </TabItem>
+        </Tabs>
+      </View>
+    </SafeAreaView>
+  );
+}
