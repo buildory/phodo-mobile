92089652d00f975d86c61d57a0d652086d658bd7
buildory <buildory.dev@gmail.com>
2025-09-17 17:51:04 +0900

    Feature/앱 버전 관리 구현 (#53)
    * chore: code-push, ts-node 설치
    
    * feat: tsconfig 적용
    
    * feat: code-push config추가
    
    * feat: index.ts entry 추가
    
    * update: index.ts를 index.js로 변경
    
    * feat: usesCleartextTraffic 추가
    
    * refactor: 불필요한 설정 제거
    
    * feat: version 관련 api 추가
    
    * feat: version check hook 구현
    
    * feat: layout에 codepush 적용
    
    * update: code push 비활성화, 네이티브 버전만 대응

---
+++ shared/hooks/useCheckAppVersion.ts
@@ -0,0 +1,52 @@
+import Constants from "expo-constants";
+import { useEffect } from "react";
+import {
+  getLatestAppVersion,
+  checkCodePushUpdate,
+  redirectToAppStore,
+} from "../api/version";
+import { Alert, Platform } from "react-native";
+
+const parseVersion = (v: string) => {
+  return v.split(".").map((n) => parseInt(n, 10));
+};
+const isVersionLessThan = (a: string, b: string) => {
+  const [A1 = 0, A2 = 0, A3 = 0] = parseVersion(a);
+  const [B1 = 0, B2 = 0, B3 = 0] = parseVersion(b);
+  if (A1 !== B1) return A1 < B1;
+  if (A2 !== B2) return A2 < B2;
+  return A3 < B3;
+};
+
+export const useCheckAppVersion = () => {
+  useEffect(() => {
+    const currentVersion = Constants.expoConfig?.version as string;
+    console.log("현재 앱 버전:", currentVersion);
+
+    getLatestAppVersion(Platform.OS).then(
+      (latestAppVersionInfo) => {
+        console.log("최신 앱 버전 정보:", latestAppVersionInfo);
+        if (
+          latestAppVersionInfo.forceUpdate &&
+          isVersionLessThan(currentVersion, latestAppVersionInfo.minSupportedVersion)
+        ) {
+          Alert.alert(
+            "업데이트 필요",
+            "최신 버전으로 업데이트해야 정상적으로 이용할 수 있습니다.",
+            [
+              {
+                text: "업데이트",
+                onPress: () => redirectToAppStore(latestAppVersionInfo.storeUrl),
+              },
+              {
+                text: "취소",
+                style: "cancel",
+              },
+            ],
+            { cancelable: false }
+          );
+        }
+      }
+    );
+  }, []);
+};
